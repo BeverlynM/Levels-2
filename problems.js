51762c1990fb1075e4b47fed53d9918b8180edc9
Beverlyn Medina <bevm@Beverlyns-MacBook-Air.local>
2022-07-12 11:05:32 -0400

    Final

--- problems.js
+++ problems.js
@@ -8,111 +8,78 @@
     }
  }
 
- console.log(min(-45, 45));
+ console.log(min(-6, 45));
 
 
 
 
 
- //Easy - was confused on arrays
- function Students(firstname, lastname, age){
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.age = age;
- }
+//  //Easy - was confused on arrays
+//  function Students(firstname, lastname, age){
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//     this.age = age;
+//  }
 
 
- var myClass = [
-    new Student("Beverlyn", "Beringer", 19)
-    new Student("Mariana", "Medina", 13),
-    new Student("Alexis", "John", 2)
- ];
+//  var myClass = [
+//     new Student("Beverlyn", "Beringer", 19),
+//     new Student("Mariana", "Medina", 13),
+//     new Student("Alexis", "John", 2)
+//  ];
 
+
+ var students = [
+    {
+    firstName: 'Bev',
+    lastName: 'Beringer',
+    age: 19
+    },
+    {
+    firstName: 'Mariana',
+    lastName: 'Medina',
+    age: 13
+    },
+    {
+    firstName: 'Alexis',
+    lastName: 'Quiroa',
+    age: 2
+    }
+    ];
+    
+    for (var i = 0; i < students.length; i++) {
+    console.log('Hello, my name is ' + students[i].firstName + ' ' + students[i].lastName + ' and I am ' + students[i].age + ' years old');
+    }
 
 
 
 
  //Medium
- var mos = prompt("Please enter a number: ");
- months();
- function months(mos)
-     {
-         switch (mos) {
-         case 1:
-             console.log("January");
-             break;
-         case 2:
-             console.log("February");
-             break;
-         case 3:
-             console.log("March");
-             break;
-         case 4:
-             console.log("April");
-             break;
-         case 5:
-             console.log("May");
-             break;
-         case 6:
-             console.log("June");
-         case 7:
-             console.log("July");
-         case 8:
-             console.log("August");
-             break;
-         case 9:
-             console.log("September");
-         case 10:
-             console.log("October");
-         case 11:
-             console.log("November");
-             break;
-         case 12:
-             console.log("December");
-              break;
-         default:
-             console.log("Error, Enter a valid number");
-             break;
-         }
-     }
+ const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+ 
+ function takeInput() {
+   const inputNumber = prompt("Enter a number (1-12): ");
+   if (inputNumber < 1 || inputNumber > 12) {
+    alert("Its an invalid number!!");
+  }
+
+   
+   else {
+    console.log(inputNumber,"-", months[inputNumber - 1]);
+  }
+   // input again
+  takeInput();
+ }
 
  
  //Hard
  //BMI = mass / height ^2 = mass / (height * height)
- var Tom = {
-    name: 'Tom', 
-    mass: '8g',
-    height: '9in',
-    calcBMI: function(){
-      this.bmi = this.mass / Math.pow(this.height,2);
-    }
-  }
-  
-  Tom.calcBMI();
-  console.log(Tom);
-  
-  var Jerry = {
-    name: 'Jerry',
-    mass: '45g',
-    height: '10in',
-    calcBMI: function(){
-      this.bmi = this.mass / Math.pow(this.height,2);
-    }
-  }
-  
-  Jerry.calcBMI();
-  console.log(Jerry);
-  
-
-  
-  if(Tom.bmi > Jerry.bmi){
-    console.log(Tom.name + ' has a higher BMI');
-  }
-  else  if (Tom.bmi < Jerry.bmi){
-    console.log(Jerry.name + ' has a higher BMI');
-  } else {
-    console.log(Tom.name + ' and ' + Jerry.name + ' has the same BMI');
-  }
-
-
-
+ var Tom = [ 9, 8];
+ var Jerry = [ 10, 45];
+ var t_bmi = Tom[1] / (Tom[0] * Tom[0]);
+ var j_bmi = Jerry[1] / (Jerry[0] * Jerry[0]);
+ var answer = false;
+ if (t_bmi > j_bmi) {
+   answer = true;
+ }
+ console.log(`Is Tom's BMI higher than Jerry's? ${answer}`)
